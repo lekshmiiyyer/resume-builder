ca966f82948e225ee088e73ca7831b05db36bd6e
lekshmi L <lekshmiiyyer123@gmail.com>
2024-08-23 11:58:14 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    dirs: [
+      'stories',
+      'src/__test__',
+      'src/common',
+      'src/helpers',
+      'src/modules',
+      'src/pages',
+      'src/styles',
+      'src/templates',
+    ],
+  },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
